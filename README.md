c3185fb6d057a02a4cac27c1df8855ec0e2eac7f
のびすけ <rsksound@gmail.com>
2019-03-01 01:03:20 +0900

    init

--- README.md
+++ README.md
@@ -1,20 +1,11 @@
-LINE Messagin APIのNode-REDのノードです。
-
 ## 概要
 
-LINE Messagin APIを利用できるNode-REDのノードです。
-
-以下のAPIを利用できます。
-
-* LINE Messaging API
-    * Reply Message
-    * Push Message
-* LINE Notify
+Twitter APIでFollowを行うノードです。
 
 ## インストール
 
 ```
-npm i node-red-contrib-line-messaging-api
+npm i node-red-contrib-twitter-follow
 ```
 
 or
@@ -23,29 +14,14 @@ AdminタブからInstall
 
 ## 利用イメージ
 
-### Reply Message
-
-* HTTP inノードで受け取る
-* Functionノードでハンドリング
-* Reply Messageノードでリプライ
-
-![](https://i.gyazo.com/d3df3a28e010b008043ed80ae6a672ea.gif)
-
-### Push Message
-
-![](https://i.gyazo.com/1562a3e4539469515c798d9e3c50d052.gif)
-
-### LINE Notify
-
-![](https://i.gyazo.com/e64db6a7ee48cea43ed3c70b5fd2f05f.gif)
+WIP
 
 ## LINK
 
-* [NodeRED](https://flows.nodered.org/node/node-red-contrib-line-messaging-api)
-* [Libraries.io](https://libraries.io/npm/node-red-contrib-line-messaging-api)
-* [npm](https://www.npmjs.com/package/node-red-contrib-line-messaging-api)
+* [NodeRED](https://flows.nodered.org/node/node-red-contrib-twitter-follow)
+* [Libraries.io](https://libraries.io/npm/node-red-contrib-twitter-follow)
+* [npm](https://www.npmjs.com/package/node-red-contrib-twitter-follow)
 
 ## release
 
-* 2019/2/13: PUSH MessageとLINE Notify対応
-* 2018/10/11: 現状は簡単なリプライのみ実装されています。
+* 2019/2/28-3/1: [enebular meetup vol8](https://enebular.connpass.com/event/117432/)で勢い余って開発&公開
--- nodes/follow/follow.js
+++ nodes/follow/follow.js
@@ -1,31 +1,45 @@
 module.exports = (RED) => {
     'use strict';
-    
-    const axiosBase = require('axios');
+
+    const twitter = require('twitter');
 
     const main = function(config){
         const node = this;
         RED.nodes.createNode(node, config);
-        const LINE_TOKEN = config.AccessToken;
-        const axios = axiosBase.create({
-            baseURL: `https://notify-api.line.me`,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': `Bearer ${LINE_TOKEN}`
-            }
-        });
+        let twitterconfig;
+        try {
+            twitterconfig = require('../../env');
+        } catch (error) {
+            twitterconfig = { 
+                consumer_key: config.consumer_key,
+                consumer_secret: config.consumer_secret,
+                access_token_key: config.access_token_key,
+                access_token_secret: config.access_token_secret
+            };
+        }
+        
+        let client = {};
+        try {
+            client = new twitter(twitterconfig);
+        } catch (error) {
+            console.log(error);
+        }
 
         node.on('input', async (msg) => {
             const mes = msg.payload;
             try {
-                const res = await axios.post(`/api/notify?message=${encodeURI(mes)}`);
-                msg.payload = res.data;
-                node.send(msg);                
+                if(!msg.tweet) throw new Error('Tweetオブジェクトがありません。');
+
+                console.log(`[${msg.tweet.user.id_str}]${msg.tweet.user.name}さんをフォローしようとしています。`);
+                await client.post('friendships/create', {user_id: msg.tweet.user.id});
+                console.log(`${msg.tweet.user.name}さんをフォローしました。`);            
             } catch (error) {
                 console.log(error);
             }
-
+            msg.payload = res.data;
+            node.send(msg);
         });
+
     }
 
     RED.nodes.registerType("Notify", main);
